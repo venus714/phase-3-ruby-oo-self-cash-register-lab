88e6c8ce814aaa1e60e888c38e049b2b442daf3c
Student <venus.kabera@student.moringaschool.com>
2023-02-20 00:08:37 +0300

    completed

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,41 @@
+class CashRegister
+    attr_accessor :total, :discount
+    attr_reader 
+    attr_writer :dollars
+    
+    def initialize(discount = 0 )
+        @discount = discount
+        @total = 0
+        @items= []
+    end
+
+    def add_item(title, price, amount = 1)
+        @total += price * amount
+
+    end
+
+    def apply_discount
+        if discount > 0 
+            my_discount = @total * @discount/100
+            @total = total - my_discount
+             "After the discount, the total comes to $#{@total}."
+        else 
+            "There is no discount to apply."
+
+        end
+    end
+    def items(item, price)
+        @items << item
+    end
+    
+end
+
+
+
+
+
+
+
+
+
 
